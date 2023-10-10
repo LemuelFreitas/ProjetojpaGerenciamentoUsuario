3bc43ce7cd4f1b266b6c700930b094ba03bda906
LemuelFreitas <132360202+LemuelFreitas@users.noreply.github.com>
2023-10-10 10:46:28 -0300

    Add files via upload

---
+++ ProjetojpaUsuario/src/main/java/com/ProjetojpaUsuario/ProjetojpaUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.ProjetojpaUsuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetojpaUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetojpaUsuarioApplication.class, args);
+	}
+
+}
---
+++ ProjetojpaUsuario/src/test/java/com/ProjetojpaUsuario/ProjetojpaUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProjetojpaUsuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetojpaUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
